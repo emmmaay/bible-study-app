e2ef3ff1be2769c80c5dbbe3455ce802c70554dc
emmmaay <danvvbb84@gmail.com>
2025-08-03 01:02:14 +0100

    Create icons.tsx

---
+++ client/src/components/ui/icons.tsx
@@ -0,0 +1,169 @@
+import { LucideProps } from "lucide-react";
+
+export const BookIcon = (props: LucideProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M4 12h8l2-4" />
+    <path d="m2 5 10 10v5a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2Z" />
+    <path d="m14 13h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4c-1.1 0-2-.9-2-2v-6a2 2 0 0 1 2-2Z" />
+  </svg>
+);
+
+export const StudyIcon = (props: LucideProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M4 9h12v9a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2Z" />
+    <path d="M8 3h4a2 2 0 0 1 2 2v2H8a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2Z" />
+    <path d="m8 13 4 " />
+    <path d="m8 17 2 " />
+  </svg>
+);
+
+export const CrossIcon = (props: LucideProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M12 2v20" />
+    <path d="M8 6h8" />
+  </svg>
+);
+
+export const PrayIcon = (props: LucideProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M12 5L8 21l4-7 4 7-4-16" />
+    <path d="M8 12h8" />
+  </svg>
+);
+
+export const HeartIcon = (props: LucideProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.29 1.51 4.04 3 5.5l7 7Z" />
+  </svg>
+);
+
+export const ChurchIcon = (props: LucideProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M18 12h4l-6-6-6 6h4v8h4v-8Z" />
+    <path d="M10 6V4a2 2 0 1 1 4 0v2" />
+    <path d="M8 20h8" />
+    <path d="M6 16h12" />
+  </svg>
+);
+
+export const LoadingSpinner = (props: LucideProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="animate-spin"
+    {...props}
+  >
+    <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+  </svg>
+);
+
+export const OfflineIcon = (props: LucideProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M3 12h18l-9-9-9 9Z" />
+    <path d="M12 20V12" />
+    <path d="M8 16h8" />
+  </svg>
+);
+
+export const OnlineIcon = (props: LucideProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M12 2L2 7l10 5 10-5-10-5Z" />
+    <path d="M2 17l10 5 10-5" />
+    <path d="M2 12l10 5 10-5" />
+  </svg>
+);
